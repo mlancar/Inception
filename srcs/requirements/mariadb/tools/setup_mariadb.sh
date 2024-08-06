089d4f8abe16423e7a7bdc31a4bf40393df997e9
Marine LANCAR <malancar@bess-f2r5s3.clusters.42paris.fr>
2024-08-06 17:25:10 +0200

    working on mariadb config

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -2,7 +2,11 @@
 
 service mariadb start
 
+CREATE DATABASE wpdb;
+
 CREATE USER 'new_user'@'%' IDENTIFIED BY 'password';
+
+GRANT ALL ON wpdb.* to 'wpuser'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 
 service mariadb stop
 
