1f4b0df33879f9e0ab5468e5c1af968a821b2a58
root <root@malancar.malancar.virtualbox.org>
2024-08-08 18:55:03 +0200

    working on mariadb

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-
-sleep 10
-
-mysql start
-
-mysql -e "CREATE DATABASE IF NOT EXISTS Inception;"
-
-mysql -e "CREATE USER IF NOT EXISTS malancar@'%' IDENTIFIED BY password;"
-
-mysql -e "GRANT ALL PRIVILEGES ON Inception.* TO malancar@'%' IDENTIFIED BY password;"
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY password_root;"
+if [ -d /run/mysqld ]; then
+    echo "stopping mariadb"
+    rc-service mariadb stop
+    rc-service mariadb status
+    # mysqladmin -u root -p password_root shutdown
+fi
 
 
-mysql -e "FLUSH PRIVILEGES;"
+if [ -d /var/lib/mysql/Inception ]; then
+    echo "Database already exist"
+else
 
-mysqladmin -u root -p password_root shutdown
+    sleep 2
 
+    /etc/init.d/mariadb setup
+    rc-service mariadb start
+    sleep 2
+
+    echo "CREATE DATABASE IF NOT EXISTS \`Inception\`;" > db.sql
+
+    echo "CREATE USER IF NOT EXISTS \`malancar\`@'%' IDENTIFIED BY 'password';" >> db.sql
+
+    echo "GRANT ALL PRIVILEGES ON \`Inception\`.* TO \`malancar\`@'%' IDENTIFIED BY 'password';" >> db.sql
+  
+    echo "ALTER USER \`root\`@'localhost' IDENTIFIED BY 'password_root';" >> db.sql
+   
+    echo "FLUSH PRIVILEGES;" >> db.sql
+
+    mysql < db.sql
+
+    mysqladmin -u root -ppassword_root shutdown
+    
+fi
 exec mysqld_safe
